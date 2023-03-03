ce7c52ca7d8c8fd522aa9e2df5e0b0d7874d2491
MelarcAB <melarc.ab@gmail.com>
2023-03-03 21:06:52 +0100

    deshago cambios

--- app/Http/Controllers/SocketController.php
+++ app/Http/Controllers/SocketController.php
@@ -481,7 +481,7 @@ class SocketController extends Controller implements MessageComponentInterface
         $this->rooms[$room_slug][$conn->resourceId] = [
             'conn' => $conn,
             'username' => $user->username,
-            'image' => ($user->image),
+            'image' => asset($user->image),
         ];
 
 
