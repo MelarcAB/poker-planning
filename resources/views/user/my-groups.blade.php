ddcf4ca182c38d9fccd299f1aafd81b5412b12f6
MelarcAB <melarc.ab@gmail.com>
2023-03-04 15:07:48 +0100

    limpio comilla doble

--- resources/views/user/my-groups.blade.php
+++ resources/views/user/my-groups.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <h3 class="custom-title">Mis grupos</h3>"
+        <h3 class="custom-title">Mis grupos</h3>
         <div class="custom-card-container">
             @foreach($user->groups as $group)
             <a class="custom-link" href="{{route('group',['slug'=>$group->slug])}}">
