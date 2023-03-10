6c03b34616b8ff24026105a1413d200b5e3a006b
MelarcAB <melarc.ab@gmail.com>
2023-03-10 22:31:50 +0100

    Mejora visual al botón de iniciar sesión con Google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 <div class="container">
+
+    <div class="custom-card-100" style="padding:10px 15px;text-align:center">
+        <div class="card-body">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <a href="{{ route('google.login') }}" class=" custom-button custom-link"><i class="fab fa-google"></i> Iniciar sesión con Google</a>
+                <div class="g-signin2" data-width="300" data-height="200" data-longtitle="true">
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+<!--
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -10,13 +26,16 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-                        <a href="{{ route('google.login') }}">
-                            <img src="https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png" alt="Iniciar sesión con Google">
-                        </a>
+                        <a href="{{ route('google.login') }}" class="btn btn-google btn-block"><i class="fab fa-google"></i> Iniciar sesión con Google</a>
+
+                        <div class="g-signin2" data-width="300" data-height="200" data-longtitle="true">
+
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+-->
+
 @endsection
