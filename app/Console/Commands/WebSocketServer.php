59d3d949a867b5447a607d59d19844b84907d201
MelarcAB <melarc.ab@gmail.com>
2023-08-12 17:18:50 +0200

    test

--- app/Console/Commands/WebSocketServer.php
+++ app/Console/Commands/WebSocketServer.php
@@ -33,49 +33,36 @@ class WebSocketServer extends Command
      */
     public function handle()
     {
-
         try {
             $port = 8090;
+
             if (app()->environment('production')) {
-                $port = 8090;
+                // Rutas para el certificado y la clave privada.
+                $sslOptions = [
+                    'local_cert' => '/path_to_your/fullchain.pem',    // tu certificado
+                    'local_pk' => '/path_to_your/privkey.pem',        // tu clave privada
+                    'verify_peer' => false
+                ];
 
-                if (app()->environment('production')) {
-                    // Rutas para el certificado y la clave privada.
-                    // Asegúrate de que estén configuradas correctamente y sean accesibles.
-                    //WEBSOCKETS_SSL_LOCAL_CERT y WEBSOCKETS_SSL_LOCAL_PK
-                    $sslOptions = [
-                        'local_cert' => '/path_to_your/fullchain.pem',    // tu certificado
-                        'local_pk' => '/path_to_your/privkey.pem',        // tu clave privada
-                        'verify_peer' => false
-                    ];
-
-
-                    $loop = \React\EventLoop\Loop::get();
-                    $socket = new \React\Socket\Server('0.0.0.0:' . $port, $loop);
-                    $socket = new \React\Socket\SecureServer($socket, $loop, $sslOptions);
-                    $server = new IoServer(
-                        new HttpServer(
-                            new WsServer(
-                                new SocketController()
-                            )
-                        ),
-                        $socket,
-                        $loop
-                    );
-                    $server->run();
-                } else {
-                    $server = IoServer::factory(
-                        new HttpServer(
-                            new WsServer(
-                                new SocketController()
-                            )
-                        ),
-                        $port
-                    );
-                    $server->run();
-                }
+                $loop = \React\EventLoop\Loop::get();
+                $socket = new \React\Socket\Server('0.0.0.0:' . $port, $loop);
+                $socket = new \React\Socket\SecureServer($socket, $loop, $sslOptions);
+            } else {
+                $socket = new \React\Socket\Server('0.0.0.0:' . $port, $loop);
             }
+
+            $server = new IoServer(
+                new HttpServer(
+                    new WsServer(
+                        new SocketController()
+                    )
+                ),
+                $socket,
+                $loop
+            );
+            $server->run();
         } catch (Exception $e) {
+            // Consider logging more information.
             echo $e->getMessage();
         }
     }
